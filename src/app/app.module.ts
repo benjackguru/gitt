160cd73401fbc514e1ba8c50db527fce5b33c38a
BenJackGuru <100480556+BenJackGuru@users.noreply.github.com>
2022-05-14 19:49:06 +0530

    second
    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { VendorComponent } from './components/vendor/vendor.component';
+import { AddVendorComponent } from './components/add-vendor/add-vendor.component';
+import { CategoryComponent } from './components/category/category.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { SubcategoryComponent } from './components/subcategory/subcategory.component';
+import { AddSubcategoryComponent } from './components/add-subcategory/add-subcategory.component';
+import { ProductsComponent } from './components/products/products.component';
+import { AddProductsComponent } from './components/add-products/add-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopNavComponent,
+    SideNavComponent,
+    VendorComponent,
+    AddVendorComponent,
+    CategoryComponent,
+    AddCategoryComponent,
+    SubcategoryComponent,
+    AddSubcategoryComponent,
+    ProductsComponent,
+    AddProductsComponent
   ],
   imports: [
     BrowserModule,
